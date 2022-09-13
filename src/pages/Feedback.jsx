df2529519f320e0cfb157cdd6fe0114b13452d80
Udison Abreu <udi0abreu@gmail.com>
2022-09-13 15:54:59 -0300

    css feedback mobile

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faRotateRight,
+  faTrophy,
+} from '@fortawesome/free-solid-svg-icons';
 import Header from '../components/Header';
 import { NextQuestionAction, resetScoreAction } from '../Redux/action';
+import '../css/Feedback.css';
+import Logo from '../components/Logo';
 
 class Feedback extends React.Component {
   goToLogin = () => {
@@ -21,31 +28,53 @@ class Feedback extends React.Component {
     const { assertions, score } = this.props;
     const THREE = 3;
     return (
-      <div>
-        <div>
-          <Header />
-          <h1 data-testid="feedback-text">Feedback</h1>
-          { assertions < THREE
-            ? <h1 data-testid="feedback-text">Could be better...</h1>
-            : <h1 data-testid="feedback-text">Well Done!</h1>}
-
-          <p data-testid="feedback-total-question">{ assertions }</p>
-          <p data-testid="feedback-total-score">{ score }</p>
+      <div className="Feedback">
+        <Header />
+        <Logo />
+        <div className="feedback-card">
+          {assertions < THREE ? (
+            <h1 className="bad-feedback" data-testid="feedback-text">
+              Could be better...
+            </h1>
+          ) : (
+            <h1 className="good-feedback" data-testid="feedback-text">
+              Well Done!
+            </h1>
+          )}
+          <p className="feedback-assertions-container">
+            You got
+            <span
+              data-testid="feedback-total-question"
+              className="feedback-assertions"
+            >
+              {assertions}
+            </span>
+            questions right.
+          </p>
+          <p className="feedback-score-container">
+            Your Score:
+            <span
+              className="feedback-score"
+              data-testid="feedback-total-score"
+            >
+              {score}
+            </span>
+          </p>
         </div>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ this.goToLogin }
-        >
-          Play Again
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ this.goToRanking }
-        >
-          Ranking
-        </button>
+        <div className="feedback-button-container">
+          <div className="button-container">
+            <button type="button" data-testid="btn-play-again" onClick={ this.goToLogin }>
+              <FontAwesomeIcon icon={ faRotateRight } />
+            </button>
+            <p>Play Again</p>
+          </div>
+          <div className="button-container">
+            <button type="button" data-testid="btn-ranking" onClick={ this.goToRanking }>
+              <FontAwesomeIcon icon={ faTrophy } />
+            </button>
+            <p>Ranking</p>
+          </div>
+        </div>
       </div>
     );
   }
