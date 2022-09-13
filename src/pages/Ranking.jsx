adca78e78acb178b2e7d821d8035d08139bd8e55
Udison Abreu <udi0abreu@gmail.com>
2022-09-13 17:27:22 -0300

    css ranking mobile

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
 import { resetScoreAction } from '../Redux/action';
+import '../css/Ranking.css';
 
 class Ranking extends Component {
   state = {
@@ -33,22 +36,35 @@ class Ranking extends Component {
   render() {
     const { ranking } = this.state;
     return (
-      <div>
+      <div className="Ranking">
         <h1 data-testid="ranking-title">Ranking</h1>
-        <div>
-          {
-            ranking.map(({ name, score, picture }, index) => (
-              <div key={ index }>
-                <img src={ picture } alt={ `Foto ${index}` } />
-                <p data-testid={ `player-name-${index}` }>{name}</p>
-                <p data-testid={ `player-score-${index}` }>{score}</p>
-              </div>
-            ))
-          }
+        <div className="button-container">
+          <button data-testid="btn-go-home" type="button" onClick={ this.handleClick }>
+            <FontAwesomeIcon icon={ faRotateRight } />
+            Play Again
+          </button>
         </div>
-        <button data-testid="btn-go-home" type="button" onClick={ this.handleClick }>
-          Sair
-        </button>
+        <table>
+          <tbody>
+            {
+              ranking.map(({ name, score, picture }, index) => (
+                <tr key={ index }>
+                  <td className="position">{ index + 1}</td>
+                  <td className="avatar">
+                    <img src={ picture } alt={ `Foto ${index}` } />
+                  </td>
+                  <td className="name" data-testid={ `player-name-${index}` }>{name}</td>
+                  <td
+                    className="score"
+                    data-testid={ `player-score-${index}` }
+                  >
+                    {score}
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
